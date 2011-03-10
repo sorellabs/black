7ebb2ac418f34b36cea6078fa6b1ae7977456d4c
Quildreen Motta <quildreen@gmail.com>
2011-03-10 10:26:51 -0300

    Provides implementation for Array#indexOf

--- src/array.js
+++ src/array.js
@@ -14,8 +14,10 @@
 	if (typeof exports != "undefined")  fallback = require('./core').fallback
 	else                                fallback = b.lack.fallback
 
-	// Alias, erro messages and stuff
+	// Alias, error messages and stuff
 	var classOf = Object.prototype.toString
+	  , abs     = Math.abs
+	  , max     = Math.max
 	  , fallback
 
 
@@ -27,6 +29,37 @@
 	//
 	function isArray(obj) {
 		return classOf.call(obj) == '[object Array]'
+	}
+
+
+	///// Function `indexOf` /////////////////////////////////////////////////
+	//
+	//     #indexOf(value[, Num:start]) → Num
+	//
+	// Compares `value` to the elements of the array, in ascending order, using
+	// the Strict Equality Comparison algorithm. If a match is found, that
+	// match is returned, otherwise, -1 is returned.
+	// 
+	// The optional `start` argument defines the array index at which the
+	// search should start. If it's not present, or 0, the whole array will be
+    // searched, otherwise, only the part starting at the given index will be
+	// searched.
+	// 
+	// Follows the algorithm described in ES-262 15.4.4.14
+	//
+	function index_of(value, start) {
+		var obj = Object(this)
+		  , len = obj.length >> 0
+		  , key
+
+		start = +start || 0
+		if (!len || start >= len)  return -1
+		if (start < 0)             start = max(0, len - abs(start))
+		
+		for (key = start; key < len; ++key)
+			if (key in obj && obj[key] === value) return key
+
+		return -1
 	}
 
 
