37ce0bffeae8ed38d40bee940c8f9aa4bcc8f708
Quildreen Motta <quildreen@gmail.com>
2011-06-21 23:31:30 -0300

    Implements basic object handling.

--- src/object.js
+++ src/object.js
@@ -1,88 +1,82 @@
-/*****************************************************************************
- *                              ~b.lack.object~                              *
- *                            ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾                            *
- * Provides missing functionality for JavaScript vanilla Objects.            *
- *                                                                           *
- *     ________________________________________________________________      *
- *       Copyright (c) 2011 Quildreen Motta // Licenced under MIT/X11        *
- *****************************************************************************/
+/******************************************************************************
+ *                                ~black.obj~                                 *
+ *                              ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾                               *
+ * Extensions to work with objects.                                           *
+ *     _________________________________________________________________      *
+ *        Copyright (c) 2011 Quildreen Motta // Licenced under MIT/X11        *
+ ******************************************************************************/
+void function (root) { var __old, obj
 
-(function (root) {
-
-	// Get the fallback function
-	if (typeof exports != "undefined")  fallback = require('./core').fallback
-	else                                fallback = b.lack.fallback
+    , keys  = Object.keys
 
 
-	// Error message, variables, alias and such
-	var EObjExpected = "Object prototype may only be an Object or null"
-	  , ENonObjProto = "Object.getPrototypeOf called on non-object"
-	  , has          = Object.prototype.hasOwnProperty
-	  , fallback
+    // Checks if an object has a key set in itself
+    function has(obj, key) {
+        return obj.hasOwnProperty(key)
+    }
+
+    // Returns a list of values in the object
+    function values(obj) {
+        return keys(obj).map(function(key) {
+            return obj[key] })
+    }
+
+    // Returns a list of tuples (key, value) in the object
+    function items(obj) {
+        return keys(obj).map(function(key) {
+            return [key, obj[key]] })
+    }
+
+    // Returns a property in the object, or default
+    function attr(obj, key, def) {
+        return obj[key] != null? obj[key]
+                               : def
+    }
+
+    // Sets the property only if it hasn't been set yet
+    function set_default(obj, key, value) {
+        if (obj[key] == null) obj[key] = value
+        return obj[key]
+    }
+
+    // Removes a property from the object, returns it or default
+    function pop(obj, key, def) { var result
+        result = attr(obj, key, def)
+
+        delete obj[key]
+        return result
+    }
+
+    // Checks if the object is empty
+    function emptyp(obj) { var key
+        for (key in obj)
+            if (has(obj, key)) return false
+
+        return true
+    }
 
 
+
+    ///// Exports //////////////////////////////////////////////////////////////
+    if (typeof exports == 'undefined') {
+        if (!root.black) root.black = { }
 
-	///// Function `create` //////////////////////////////////////////////////
-	//
-	//     create(Obj:proto[, Obj:props]) → Obj
-	//
-	// Creates a new Object with the specified [[Prototype]] and properties.
-	//
-	function create(proto, props) {
-		if (typeof proto != "object") throw new TypeError(EObjExpected)
+        __old = root.obj.obj
+        obj = root.obj.obj = { }
 
-		var Empty = function(){ }
-		  , obj, prop
+        ///// Method obj.make_local ////////////////////////////////////////////
+        obj.make_local = function() {
+            root.obj = __old
+            return obj }}
+    else
+        obj = exports
 
-		Empty.prototype = proto
-		obj             = new Empty
-		for (prop in props)
-			if (has.call(obj, prop)) obj[prop] = props[prop]
+    ///// -Properties under obj ////////////////////////////////////////////////
+    obj.keys        = keys
+    obj.values      = values
+    obj.items       = items
+    obj.attr        = attr
+    obj.set_default = set_default
+    obj.pop         = pop
 
-		return obj
-	}
-
-
-	///// Function `getPrototypeOf` //////////////////////////////////////////
-	//
-	//     getPrototypeOf(Obj:obj) → Obj
-	//
-	// Returns the [[Prototype]] of the specified object.
-	//
-	// Kinda stolen from John Resig's blog :3
-	// http://ejohn.org/blog/objectgetprototypeof/
-	//
-	function get_proto(obj) {
-		if (typeof obj != "object") throw new ENonObjProto
-
-		return object.__proto__ || object.constructor.prototype
-	}
-
-
-	///// Function `keys` ////////////////////////////////////////////////////
-	//
-	//     keys(Obj:obj) → Array
-	//
-	// Returns an array of all **own** enumerable properties found upon
-	// a given object, in the same order as that provided by a for-in
-	// loop.
-	//
-	// > This doesn't list the properties found on the prototype chain,
-	// > as the for-in loop does.
-	//
-	function keys(obj) {
-		var rv = []
-		  , prop
-
-		for (prop in obj)
-			if (has.call(obj, prop)) rv.push(prop)
-
-		return rv
-	}
-
-
-	///// Provides the fallbacks /////////////////////////////////////////////
-	fallback(Object, { create:         create
-	                 , getPrototypeOf: get_proto
-	                 , keys:           keys })
-})(this);
+}(this)
