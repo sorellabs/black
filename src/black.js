2a58de0dba08b846faf2a4ba5ea97fa18d5df5f8
Quildreen Motta <quildreen@gmail.com>
2011-06-23 21:12:04 -0300

    Node.js support

---
+++ src/black.js
@@ -0,0 +1,13 @@
+/******************************************************************************
+ *                                  ~black~                                   *
+ *                                ‾‾‾‾‾‾‾‾‾‾‾                                 *
+ * Extended and sanitised "standard" library for JavaScript.                  *
+ *     _________________________________________________________________      *
+ *        Copyright (c) 2011 Quildreen Motta // Licenced under MIT/X11        *
+ ******************************************************************************/
+var core = require('./core')
+
+exports.unpack     = core.unpack
+exports.unpack_all = core.unpack_all
+exports.obj        = require('./obj')
+
