2fe13b3c79c3724db9bfd5a5485a01bb8c497a87
Quildreen Motta <quildreen@gmail.com>
2011-06-26 16:03:15 -0300

    Adds some function tools.

---
+++ src/fn.js
@@ -0,0 +1,61 @@
+/******************************************************************************
+ *                                    ~fn~                                    *
+ *                                  ‾‾‾‾‾‾‾‾                                  *
+ * Function utilities.                                                        *
+ *     _________________________________________________________________      *
+ *        Copyright (c) 2011 Quildreen Motta // Licenced under MIT/X11        *
+ ******************************************************************************/
+void function (root) { var __old, fn
+
+    , slice = Array.prototype.slice
+
+
+    function partial(fn) { 
+        return fn.bind.apply(fn, slice.call(arguments, 1))
+    }
+
+    function delay(fn, seconds) { var args
+        args = slice.call(arguments, 2)
+        if (args.length)  fn = fn.bind.apply(fn, args)
+
+        return setTimeout(fn, seconds * 1000)
+    }
+
+    function defer(fn) { var args
+        args = slice.call(arguments, 1)
+        return delay.apply(this, [fn, 0].concat(args))
+    }
+
+    function wrap(fn, wrapper) {
+        return function() {
+            return wrapper.apply(this, [fn].concat(arguments)) }            
+    }
+    
+
+
+    ///// Exports //////////////////////////////////////////////////////////////
+    if (typeof exports == 'undefined') {
+        __old = root.black.fn
+        fn    = root.black.fn = { }
+
+        ///// Method fn.make_local /////////////////////////////////////////////
+        fn.make_local = function() {
+            root.black.fn = __old
+            return fn }}
+    else
+        fn = exports
+
+    ///// -Properties under fn /////////////////////////////////////////////////
+    fn.partial = partial
+    fn.delay   = delay
+    fn.defer   = defer
+    fn.wrap    = wrap
+
+    fn.$box    = Function
+    fn.$proto  = Function.prototype
+    fn.$utils  = { partial: partial
+                 , delay:   delay
+                 , defer:   defer
+                 , wrap:    wrap }
+
+}(this)
