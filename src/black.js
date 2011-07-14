27af82d03b89298452d8fa4892713ce27979a535
Quildreen Motta <quildreen@gmail.com>
2011-07-13 22:15:18 -0300

    Updates black package entry-point.

--- src/black.js
+++ src/black.js
@@ -5,9 +5,6 @@
  *     _________________________________________________________________      *
  *        Copyright (c) 2011 Quildreen Motta // Licenced under MIT/X11        *
  ******************************************************************************/
-var core = require('./core')
 
-exports.unpack     = core.unpack
-exports.unpack_all = core.unpack_all
-exports.obj        = require('./obj')
-
+exports.obj  = require('./obj')
+exports.list = require('./list')
